22d6a14629387583d04970e5415aac76ab8ea9b1
Yoonha Choe <48667973+yoonhachoe@users.noreply.github.com>
2021-05-29 21:12:46 +0200

    Update lang_module.py

--- models/lang_module.py
+++ models/lang_module.py
@@ -4,7 +4,6 @@ import torch
 import torch.nn as nn
 
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
-from models.selfattention import  SelfAttention
 
 class LangModule(nn.Module):
     def __init__(self, num_text_classes, use_lang_classifier=True, use_bidir=False, 
@@ -23,8 +22,6 @@ class LangModule(nn.Module):
         )
         lang_size = hidden_size * 2 if self.use_bidir else hidden_size
 
-        self.selfattention = SelfAttention(lang_size)
-
         # language classifier
         if use_lang_classifier:
             self.lang_cls = nn.Sequential(
@@ -42,20 +39,14 @@ class LangModule(nn.Module):
         lang_feat = pack_padded_sequence(word_embs, data_dict["lang_len"], batch_first=True, enforce_sorted=False)
     
         # encode description
-        gru_out, _ = self.gru(lang_feat) # B, T, H
-        # _, lang_last = self.gru(lang_feat)
-        # lang_last = lang_last.permute(1, 0, 2).contiguous().flatten(start_dim=1) # batch_size, hidden_size * num_dir
-
-        attention_value = self.selfattention(gru_out)
+        _, lang_last = self.gru(lang_feat)
+        lang_last = lang_last.permute(1, 0, 2).contiguous().flatten(start_dim=1) # batch_size, hidden_size * num_dir
 
         # store the encoded language features
-        # data_dict["lang_emb"] = lang_last # B, hidden_size
-        data_dict["lang_emb"] = attention_value  # B, H
-
-
+        data_dict["lang_emb"] = lang_last # B, hidden_size
+        
         # classify
         if self.use_lang_classifier:
             data_dict["lang_scores"] = self.lang_cls(data_dict["lang_emb"])
 
         return data_dict
-
