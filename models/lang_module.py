25ca9c0a346cad890e79b687dcc523c24de6e7ac
Yoonha Choe <48667973+yoonhachoe@users.noreply.github.com>
2021-06-10 09:53:47 +0200

    Update lang_module.py

--- models/lang_module.py
+++ models/lang_module.py
@@ -6,11 +6,10 @@ import torch.nn as nn
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 from models.selfattention import SelfAttention
 
-
 class LangModule(nn.Module):
-    def __init__(self, num_text_classes, use_lang_classifier=True, use_bidir=False,
-                 emb_size=300, hidden_size=256):
-        super().__init__()
+    def __init__(self, num_text_classes, use_lang_classifier=True, use_bidir=False, 
+        emb_size=300, hidden_size=256):
+        super().__init__() 
 
         self.num_text_classes = num_text_classes
         self.use_lang_classifier = use_lang_classifier
@@ -31,23 +30,24 @@ class LangModule(nn.Module):
                 nn.Dropout()
             )
 
+
     def forward(self, data_dict):
         """
         encode the input descriptions
         """
         word_embs = data_dict["lang_feat"]
         lang_feat = pack_padded_sequence(word_embs, data_dict["lang_len"], batch_first=True, enforce_sorted=False)
-
+    
         # encode description
-        feats, _ = self.gru(lang_feat)  # tensor containing the output features h_t from the last layer of the GRU, for each t #seq_len, batch, num_directions * hidden_size
-        feats, _ = pad_packed_sequence(feats, batch_first=True)  # B, T, H
+        feats, _ = self.gru(lang_feat) #tensor containing the output features h_t from the last layer of the GRU, for each t #seq_len, batch, num_directions * hidden_size
+        feats, _ = pad_packed_sequence(feats, batch_first=True) # B, T, H
 
         # self attention
         lang_last = self.attention(feats)
 
         # store the encoded language features
-        data_dict["lang_emb"] = lang_last  # B, H
-
+        data_dict["lang_emb"] = lang_last # B, H
+        
         # classify
         if self.use_lang_classifier:
             data_dict["lang_scores"] = self.lang_cls(data_dict["lang_emb"])
