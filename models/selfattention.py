7447db11fefb19aeefc645407dea789b0c01e4ec
Yoonha Choe <48667973+yoonhachoe@users.noreply.github.com>
2021-06-10 09:51:40 +0200

    Update selfattention.py

--- models/selfattention.py
+++ models/selfattention.py
@@ -14,6 +14,6 @@ class SelfAttention(nn.Module):
         attention_score = self.fc1(feats) # B, T, H
         attention_weight = nn.functional.softmax(attention_score, dim=1)
         attention_value = torch.bmm(attention_weight.permute(0, 2, 1).contiguous(), feats) # B, H, H
-        attention_value = self.fc2(attention_value, 1) # B, H
+        attention_value = self.fc2(attention_value) # B, H
 
         return attention_value
