588c9e4e1d209968e5ccddc7aed4906ddd215afa
Yoonha Choe <yuniqueha@gmail.com>
2021-06-18 10:00:05 +0200

    Update match_module.py

--- models/match_module.py
+++ models/match_module.py
@@ -1,19 +1,36 @@
 import torch
 import torch.nn as nn
+from models.dgcnn import DGCNN
+
 
 class MatchModule(nn.Module):
-    def __init__(self, num_proposals=256, lang_size=256, hidden_size=128):
+    def __init__(self, num_proposals=256, lang_size=256, hidden_size=128, use_cross_attn=False, use_dgcnn=False): # change hidden_size to 256 later
         super().__init__() 
 
         self.num_proposals = num_proposals
         self.lang_size = lang_size
         self.hidden_size = hidden_size
-        
-        self.fuse = nn.Sequential(
-            nn.Conv1d(self.lang_size + 128, hidden_size, 1),
-            nn.ReLU()
-        )
-        # self.match = nn.Conv1d(hidden_size, 1, 1)
+        self.use_dgcnn = use_dgcnn
+        self.use_cross_attn = use_cross_attn
+
+        if self.use_cross_attn:
+            self.cross = nn.Sequential(
+                nn.Conv1d(self.lang_size, hidden_size, 1),
+                nn.ReLU()
+            )
+        else:
+            self.fuse = nn.Sequential(
+                nn.Conv1d(self.lang_size + 128, hidden_size, 1),
+                nn.ReLU()
+            )
+
+        if self.use_dgcnn:
+            self.graph = DGCNN(
+                input_dim=128, #change later to 256
+                output_dim=128, #change later to 256
+                k=20
+            )
+
         self.match = nn.Sequential(
             nn.Conv1d(hidden_size, hidden_size, 1),
             nn.ReLU(),
@@ -41,20 +58,39 @@ class MatchModule(nn.Module):
         lang_feat = data_dict["lang_emb"] # batch_size, lang_size
         lang_feat = lang_feat.unsqueeze(1).repeat(1, self.num_proposals, 1) # batch_size, num_proposals, lang_size
 
-        # fuse
-        features = torch.cat([features, lang_feat], dim=-1) # batch_size, num_proposals, 128 + lang_size
-        features = features.permute(0, 2, 1).contiguous() # batch_size, 128 + lang_size, num_proposals
+        # DGCNN
 
-        # fuse features
-        features = self.fuse(features) # batch_size, hidden_size, num_proposals
-        
-        # mask out invalid proposals
-        objectness_masks = objectness_masks.permute(0, 2, 1).contiguous() # batch_size, 1, num_proposals
-        features = features * objectness_masks
+        if self.use_dgcnn:
+            features = self.graph(features) # batch_size, hidden_size, num_proposals
 
-        # match
-        confidences = self.match(features).squeeze(1) # batch_size, num_proposals
-                
+        # dimension reduction for language features
+        if self.use_cross_attn:
+            lang_feat = lang_feat.permute(0, 2, 1).contiguous() # batch_size, lang_size, num_proposals
+            lang_feat = self.cross(lang_feat) # batch_size, hidden_size, num_proposals
+
+            # cross attention
+            attention = torch.bmm(features.permute(0, 2, 1).contiguous(), lang_feat) # batch_size, num_proposals, num_proposals
+            attention = nn.functional.softmax(attention, dim=2)
+            attention_value = torch.bmm(graph_output.permute(0, 2, 1).contiguous(), attention)  # batch_size, hidden_size, num_proposals
+
+            # mask out invalid proposals
+            objectness_masks = objectness_masks.permute(0, 2, 1).contiguous()  # batch_size, 1, num_proposals
+            attention_value = attention_value * objectness_masks  # batch_size, hidden_size, num_proposals
+            # match
+            confidences = self.match(attention_value).squeeze(1) # batch_size, num_proposals
+        else:
+            features = torch.cat([features, lang_feat], dim=-1) # batch_size, num_proposals, 128 + lang_size
+            features = features.permute(0, 2, 1).contiguous() # batch_size, 128 + lang_size, num_proposals
+
+            # fuse features
+            features = self.fuse(features) # batch_size, hidden_size, num_proposals
+            objectness_masks = objectness_masks.permute(0, 2, 1).contiguous() # batch_size, 1, num_proposals
+            features = features * objectness_masks
+
+             # match
+            confidences = self.match(features).squeeze(1) # batch_size, num_proposals
+
+
         data_dict["cluster_ref"] = confidences
 
         return data_dict
