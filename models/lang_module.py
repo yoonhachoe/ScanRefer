843db8b19cab947b277b448d189293819dbfe8c4
Yoonha Choe <yuniqueha@gmail.com>
2021-06-20 17:25:45 +0200

    Update lang_module.py

--- models/lang_module.py
+++ models/lang_module.py
@@ -8,13 +8,14 @@ from models.selfattention import SelfAttention
 
 
 class LangModule(nn.Module):
-    def __init__(self, num_text_classes, use_lang_classifier=True, use_bidir=False,
+    def __init__(self, num_text_classes, use_lang_classifier=True, use_bidir=False, use_self_attn=False,
                  emb_size=300, hidden_size=256):
         super().__init__()
 
         self.num_text_classes = num_text_classes
         self.use_lang_classifier = use_lang_classifier
         self.use_bidir = use_bidir
+        self.use_self_attn = use_self_attn
 
         self.gru = nn.GRU(
             input_size=emb_size,
@@ -23,8 +24,11 @@ class LangModule(nn.Module):
             bidirectional=self.use_bidir
         )
         lang_size = hidden_size * 2 if self.use_bidir else hidden_size
-
-        self.attention = SelfAttention(lang_size)
+        if use_self_attn:
+            self.attention = SelfAttention(lang_size)
+            self.fc_key = nn.Sequential(
+                nn.Linear(lang_size, lang_size)
+            )
 
         # language classifier
         if use_lang_classifier:
@@ -43,14 +47,17 @@ class LangModule(nn.Module):
         word_embs = word_embs[sorted_idx]
         lang_feat = pack_padded_sequence(word_embs, data_dict["lang_len"], batch_first=True, enforce_sorted=False)
 
-        # encode description
-        feats, _ = self.gru(lang_feat)
-        feats, _ = pad_packed_sequence(feats, batch_first=True)  # batch, timestep, hidden_size
-        _, unsorted_idx = sorted_idx.sort() # unsort in original order
-        feats = feats[unsorted_idx]
-
-        # self attention
-        lang_last = self.attention(feats, input_lengths)
+        if use_self_attn:
+            # encode description
+            feats, _ = self.gru(lang_feat)
+            feats, _ = pad_packed_sequence(feats, batch_first=True)  # batch, timestep, hidden_size
+            _, unsorted_idx = sorted_idx.sort() # unsort in original order
+            feats = feats[unsorted_idx]
+            # self attention
+            lang_last = self.attention(feats)
+        else:
+            _, lang_last = self.gru(lang_feat)
+            lang_last = lang_last.permute(1, 0, 2).contiguous().flatten(start_dim=1)  # batch_size, hidden_size * num_dir
 
         # store the encoded language features
         data_dict["lang_emb"] = lang_last  # batch, hidden_size
