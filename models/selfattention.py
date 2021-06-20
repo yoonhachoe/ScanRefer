649c0b318542fc9da10679bdc77be552898b4e2d
Yoonha Choe <yuniqueha@gmail.com>
2021-06-20 17:25:31 +0200

    Update selfattention.py

--- models/selfattention.py
+++ models/selfattention.py
@@ -7,25 +7,16 @@ class SelfAttention(nn.Module):
         super().__init__()
 
         self.hidden_size = hidden_size
-        self.fc1 = nn.Linear(hidden_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, 1)
+        self.fc = nn.Linear(hidden_size, hidden_size)
 
-    def forward(self, feats, lengths):
-        attention = self.fc1(feats) # B, T, H
-        # mask attention scores
-        B, S, _ = attention.size()  # S = len longest hiddenstate
-        idx = lengths.new_tensor(torch.arange(0, S).unsqueeze(0).repeat(B, 1)).long()  # clone lengths tensor
-        lengths = lengths.unsqueeze(1).repeat(1, S)
+    def forward(self, feats):
+        score = self.fc(feats) # B, T, H
+        score = torch.bmm(score, feats.permute(0, 2, 1).contiguous())  # B, T, T
+        B, T, _ = feats.size()
+        mask = feats[:,:,0].repeat(1, T) # B, T, T
+        score = score.masked_fill(mask==0, -1e9)
+        weight = nn.functional.softmax(score, dim=-1) # B, T, T
+        value = torch.bmm(weight, feats) # B, T, H
+        value = torch.sum(value, 1) # B, H, 1
 
-        mask = (idx >= lengths)
-        mask = mask.unsqueeze(2).repeat(1, 1, self.hidden_size)
-        attention.masked_fill_(mask, float('-1e30'))  # attn mask
-
-        # softmax
-        weights = nn.functional.softmax(attention, dim=1)  # B, T, H
-        weights = weights.transpose(1, 2)  # B, H, T
-        value = torch.bmm(weights, feats) # B, H, H
-        value = value.permute(0, 2, 1).contiguous()
-        value = self.fc2(value)  # B, H, 1
-        value = torch.squeeze(value)
         return value
