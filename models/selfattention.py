54a2b702d5180e4da2e6e3aecf28c11e28476a3e
Yoonha Choe <yuniqueha@gmail.com>
2021-06-22 19:46:57 +0200

    Update selfattention.py

--- models/selfattention.py
+++ models/selfattention.py
@@ -3,23 +3,19 @@ import torch.nn as nn
 
 
 class SelfAttention(nn.Module):
-    def __init__(self, hidden_size, num_timestep):
+    def __init__(self, hidden_size):
         super().__init__()
 
         self.hidden_size = hidden_size
-        self.num_timestep = num_timestep
-        self.fc1 = nn.Linear(hidden_size, hidden_size)
-        self.fc2 = nn.Linear(num_timestep, 1)
+        self.fc = nn.Linear(hidden_size, hidden_size)
 
     def forward(self, feats):
-        score = self.fc1(feats) # B, T, H
+        score = self.fc(feats) # B, T, H
         score = torch.bmm(score, feats.permute(0, 2, 1).contiguous())  # B, T, T
         _, _, T = score.size()
         mask = feats[:,:,0].repeat(T, 1, 1).permute(1, 2, 0).contiguous() # B, T, T
         score = score.masked_fill(mask==0, -1e9)
         weight = nn.functional.softmax(score, dim=-1) # B, T, T
         value = torch.bmm(weight, feats) # B, T, H
-        value = self.fc2(value.permute(0, 2, 1).contiguous()) # B, H, 1
-        #value = torch.sum(value, 1) # B, H, 1
-
+        #value = torch.sum(value, 1) # B, H
         return value
