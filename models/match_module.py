f825b88c0818b80c741d748e40a2a1f7c72d4798
Yoonha Choe <yuniqueha@gmail.com>
2021-06-22 17:12:05 +0200

    Update match_module.py

--- models/match_module.py
+++ models/match_module.py
@@ -23,7 +23,7 @@ class MatchModule(nn.Module):
 
         if self.use_dgcnn:
             self.graph = DGCNN(
-                input_dim=self.lang_size + 128,
+                input_dim=128,
                 output_dim=128,
                 k=10
             )
@@ -55,16 +55,21 @@ class MatchModule(nn.Module):
         lang_feat = data_dict["lang_emb"] # batch_size, lang_size
         lang_feat = lang_feat.unsqueeze(1).repeat(1, self.num_proposals, 1) # batch_size, num_proposals, lang_size
 
-        # fuse
+        #if self.use_cross_attn:
+        #    features = features.permute(0, 2, 1).contiguous()  # batch_size, 128, num_proposals
+        #else:
+        #    features = torch.cat([features, lang_feat], dim=-1)  # batch_size, num_proposals, 128 + lang_size
+        #    features = features.permute(0, 2, 1).contiguous()  # batch_size, 128 + lang_size, num_proposals
+        #    # fuse features
+        #    features = self.fuse(features)  # batch_size, hidden_size, num_proposals
         features = torch.cat([features, lang_feat], dim=-1)  # batch_size, num_proposals, 128 + lang_size
         features = features.permute(0, 2, 1).contiguous()  # batch_size, 128 + lang_size, num_proposals
-        if not self.use_dgcnn:
-            # fuse features
-            features = self.fuse(features)  # batch_size, hidden_size, num_proposals
+        # fuse features
+        features = self.fuse(features)  # batch_size, hidden_size, num_proposals
 
         # mask out invalid proposals
         objectness_masks = objectness_masks.permute(0, 2, 1).contiguous()  # batch_size, 1, num_proposals
-        features = features * objectness_masks  # batch_size, 128 + lang_size, num_proposals
+        features = features * objectness_masks  # batch_size, 128(hidden_size), num_proposals
 
         # DGCNN
         if self.use_dgcnn:
