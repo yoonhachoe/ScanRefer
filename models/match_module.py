5ff357d27de71d136eeda32faff17d487308da5b
Yoonha Choe <yuniqueha@gmail.com>
2021-06-25 01:31:55 +0200

    Update match_module.py

--- models/match_module.py
+++ models/match_module.py
@@ -94,7 +94,7 @@ class MatchModule(nn.Module):
             features = self.cross2(features.permute(0, 2, 1).contiguous()) # batch_size, num_proposals, hidden_size
             score = torch.bmm(features, lang_cross.permute(0, 2, 1).contiguous()) # batch_size, num_proposals, timestep
             weight = nn.functional.softmax(score, dim=2)
-            value = torch.bmm(weight, lang_feat) # batch_size, num_proposals, hidden_size
+            value = torch.bmm(weight, lang_cross) # batch_size, num_proposals, hidden_size
             # fuse
             features = torch.cat([value, lang_feat], dim=-1)  # batch_size, num_proposals, hidden_size + lang_size
             features = features.permute(0, 2, 1).contiguous()  # batch_size, hidden_size + lang_size, num_proposals
