03b8eacb8ab21f3742a653d7804bba633ac7af78
Yoonha Choe <yuniqueha@gmail.com>
2021-06-25 19:59:50 +0200

    Update match_module.py

--- models/match_module.py
+++ models/match_module.py
@@ -22,6 +22,11 @@ class MatchModule(nn.Module):
             nn.ReLU()
         )
 
+        self.fuse2 = nn.Sequential(
+            nn.Conv1d(self.hidden_size*2, hidden_size, 1),
+            nn.ReLU()
+        )
+
         self.graph = DGCNN(
             input_dim=self.fuse_before*self.lang_size + 3 + 128,
             output_dim=128,
@@ -100,9 +105,9 @@ class MatchModule(nn.Module):
             score = torch.bmm(features_cross, lang_cross.permute(0, 2, 1).contiguous()) # batch_size, num_proposals, timestep
             weight = nn.functional.softmax(score, dim=2)
             value = torch.bmm(weight, lang_cross) # batch_size, num_proposals, hidden_size
-            #value = value + features.permute(0, 2, 1).contiguous() # batch_size, num_proposals, hidden_size
-            value = torch.cat([value, lang_feat], dim=-1) # batch_size, num_proposals, hidden_size + lang_size
-            value = self.fuse(value.permute(0, 2, 1).contiguous()) # batch_size, hidden_size, num_proposals
+            value = value + features.permute(0, 2, 1).contiguous() # batch_size, num_proposals, hidden_size
+            #value = torch.cat([value, features], dim=-1) # batch_size, num_proposals, 2*hidden_size
+            #value = self.fuse2(value.permute(0, 2, 1).contiguous()) # batch_size, hidden_size, num_proposals
             value = value * objectness_masks
             #match
             confidences = self.match(value).squeeze(1) # batch_size, num_proposals
