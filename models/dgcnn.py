db49cf5bbca0bca1af230cd22a784fd1e6e386e0
Yoonha Choe <yuniqueha@gmail.com>
2021-06-25 21:17:35 +0200

    Update dgcnn.py

--- models/dgcnn.py
+++ models/dgcnn.py
@@ -18,11 +18,12 @@ def knn(x, k):
 
 def get_graph_feature(x, k=20, idx=None):
     batch_size = x.size(0)
+    device = x.device
     num_points = x.size(2)
     x = x.view(batch_size, -1, num_points)
     if idx is None:
         idx = knn(x, k=k)   # (batch_size, num_points, k)
-    device = torch.device('cuda')
+    #device = torch.device('cuda')
 
     idx_base = torch.arange(0, batch_size, device=device).view(-1, 1, 1)*num_points
 
