0a6ec4fd872f94cf8736cdbaa17d8cf6bb87e318
Yoonha Choe <yuniqueha@gmail.com>
2021-06-25 13:19:54 +0200

    Update selfattention.py

--- models/selfattention.py
+++ models/selfattention.py
@@ -12,9 +12,9 @@ class SelfAttention(nn.Module):
     def forward(self, feats):
         score = self.fc(feats) # B, T, H
         score = torch.bmm(score, feats.permute(0, 2, 1).contiguous())  # B, T, T
-        #_, _, T = score.size()
-        #mask = feats[:,:,0].repeat(T, 1, 1).permute(1, 2, 0).contiguous() # B, T, T
-        #score = score.masked_fill(mask==0, -1e9)
+        _, _, T = score.size()
+        mask = feats[:,:,0].repeat(T, 1, 1).permute(1, 2, 0).contiguous() # B, T, T
+        score = score.masked_fill(mask==0, -1e9)
         weight = nn.functional.softmax(score, dim=2) # B, T, T
         #value = torch.bmm(weight, feats) # B, T, H
         #value = torch.max(value, 1) # B, H
