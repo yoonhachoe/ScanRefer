f56390ed73e9b1375ba9cf952dfcfc25e7d55706
Yoonha Choe <yuniqueha@gmail.com>
2021-06-26 15:24:04 +0200

    Update match_module.py

--- models/match_module.py
+++ models/match_module.py
@@ -4,39 +4,34 @@ from models.dgcnn import DGCNN
 
 
 class MatchModule(nn.Module):
-    def __init__(self, num_proposals=256, lang_size=256, hidden_size=128, use_cross_attn=False, use_dgcnn=False):
-        super().__init__() 
-
+    def __init__(self, num_proposals=256, lang_size=256, hidden_size=256, use_cross_attn=False, use_dgcnn=False):
+        super().__init__()
+        # hidden_size=128
         self.num_proposals = num_proposals
         self.lang_size = lang_size
         self.hidden_size = hidden_size
         self.use_dgcnn = use_dgcnn
         self.use_cross_attn = use_cross_attn
 
-        self.cross1 = nn.Linear(self.lang_size, hidden_size)
-        self.cross2 = nn.Linear(self.hidden_size+self.lang_size, hidden_size)
+        #self.cross1 = nn.Linear(self.lang_size, hidden_size)
+        #self.cross2 = nn.Linear(self.hidden_size+self.lang_size, hidden_size)
 
-        self.cross = nn.Linear(self.hidden_size, hidden_size)
+        self.skip = nn.Sequential(
+            nn.Conv1d(self.lang_size + 128, self.hidden_size, 1),
+        )
+
+        self.graph = DGCNN(
+            input_dim=self.lang_size + self.hidden_size,
+            output_dim=self.hidden_size,
+            k=6
+        )
 
         self.fuse = nn.Sequential(
             nn.Conv1d(self.lang_size + 128, hidden_size, 1),
             nn.ReLU()
         )
 
-        self.fuse2 = nn.Sequential(
-            nn.Conv1d(self.hidden_size*2, hidden_size, 1),
-            nn.ReLU()
-        )
-
-        self.graph = DGCNN(
-            input_dim= self.lang_size + 128,
-            output_dim=hidden_size,
-            k=6
-        )
-
-        self.skip = nn.Sequential(
-            nn.Conv1d(self.lang_size + 128, hidden_size, 1),
-        )
+        self.cross = nn.Linear(self.lang_size, self.lang_size)
 
         self.match = nn.Sequential(
             nn.Conv1d(hidden_size, hidden_size, 1),
@@ -73,8 +68,8 @@ class MatchModule(nn.Module):
             # mask out invalid proposals
             objectness_masks = objectness_masks.permute(0, 2, 1).contiguous()  # batch_size, 1, num_proposals
             features = features * objectness_masks  # batch_size, 128 + lang_size, num_proposals
-            skipfeatures = self.skip(features)  # batch_size, 128, num_proposals
-            features = self.graph(features) + skipfeatures  # batch_size, 128, num_proposals
+            skipfeatures = self.skip(features)  # batch_size, hidden_size, num_proposals
+            features = self.graph(features) + skipfeatures  # batch_size, hidden_size, num_proposals
         else: #no graph
             # fuse
             features = torch.cat([features, lang_feat], dim=-1)  # batch_size, num_proposals, 128 + lang_size
@@ -86,14 +81,21 @@ class MatchModule(nn.Module):
             features = features * objectness_masks  # batch_size, 128, num_proposals
 
         if self.use_cross_attn:
-            score = self.cross(features.permute(0, 2, 1).contiguous()) # batch_size, num_proposals, hidden_size
-            score = torch.bmm(score, features)
+            features = features.permute(0, 2, 1).contiguous()  # batch_size, num_proposals, hidden_size
+            _, P, _ = features.size()
+            lang_token = data_dict["attn_value"] # batch_size, timestep, lang_size
+            _, T, _ = lang_token.size()
+            features = torch.cat([features, lang_token], dim=1)  # batch_size, num_proposals + timestep, hidden_size
+            score = self.cross(features) # batch_size, num_proposals + timestep, lang_size
+            score = torch.bmm(score, features.permute(0, 2, 1).contiguous()) # batch_size, num_proposals + timestep, num_proposals + timestep
             weight = nn.functional.softmax(score, dim=2)
-            value = torch.bmm(weight, features.permute(0, 2, 1).contiguous()) # batch_size, num_proposals, hidden_size
-            value = value + features.permute(0, 2, 1).contiguous() # batch_size, num_proposals, hidden_size
-            value = value.permute(0, 2, 1).contiguous() # batch_size, hidden_size, num_proposals
+            value = torch.bmm(weight, features.permute(0, 2, 1).contiguous()) # batch_size, num_proposals + timestep, hidden_size
+            final_features = value[:,[0,P-1],:] # b, p, hidden_size
+            final_lang = value[:,[P,P+T-1],:] # b, t, lang_size
+            final_lang = torch.max(final_lang, 1) # b, 1, lang_size
             #match
-            confidences = self.match(value).squeeze(1) # batch_size, num_proposals
+            confidences = torch.bmm(final_features, final_lang),squeeze(2) # b, p
+            #confidences = self.match(value).squeeze(1) # batch_size, num_proposals
         else:
              # match
             confidences = self.match(features).squeeze(1) # batch_size, num_proposals
