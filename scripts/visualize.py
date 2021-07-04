2ae39ac1861a2b34ee2084fa2c44f61dd3e18260
Yoonha Choe <yuniqueha@gmail.com>
2021-07-04 10:51:09 +0200

    Update visualize.py

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -438,7 +438,7 @@ def colorize(args, scanrefer, data, config):
         cmap = matplotlib.cm.Blues
         template = '<span class="barcode"; style="color: black; background-color: {}">{}</span>'
         colored_string = ''
-        for word, color in zip(token, data["attn_weight"][i].cpu()):
+        for word, color in zip(token, data["attn_weight"][i].tolist()):
             color = matplotlib.colors.rgb2hex(cmap(color)[:3])
             colored_string += template.format(color, '&nbsp' + word + '&nbsp')
 
